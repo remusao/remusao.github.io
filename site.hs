848d4d99e6f1ab61b32868e4215b7c835e7b5342
Rémi Berson <remi@cliqz.com>
2017-10-07 19:40:25 +0200

    Improve template

--- site.hs
+++ site.hs
@@ -41,20 +41,13 @@ main = do
               >>= loadAndApplyTemplate "templates/default.html" postCtx
               >>= relativizeUrls
 
-      create ["archive.html"] $ do
-          route idRoute
-          compile $ do
-              posts <- recentFirst =<< loadAll "posts/*"
-              let archiveCtx =
-                      listField "posts" postCtx (return posts) `mappend`
-                      constField "title" "Archives"            `mappend`
-                      defaultContext
-
-              makeItem ""
-                  >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                  >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                  >>= relativizeUrls
-
+      match "notes/*" $ do
+          route $ setExtension "html"
+          compile $ (pandocCompilerWithTransformM defaultHakyllReaderOptions defaultHakyllWriterOptions
+                  $ (renderFormulae defaultPandocFormulaOptions))
+              >>= loadAndApplyTemplate "templates/note.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
 
       match "index.html" $ do
           route idRoute
@@ -63,6 +56,20 @@ main = do
               let indexCtx =
                       listField "posts" postCtx (return posts) `mappend`
                       constField "title" "Home"                `mappend`
+                      defaultContext
+
+              getResourceBody
+                  >>= applyAsTemplate indexCtx
+                  >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                  >>= relativizeUrls
+
+      match "notes.html" $ do
+          route idRoute
+          compile $ do
+              posts <- recentFirst =<< loadAll "notes/*"
+              let indexCtx =
+                      listField "notes" postCtx (return posts) `mappend`
+                      constField "title" "Notes"               `mappend`
                       defaultContext
 
               getResourceBody
