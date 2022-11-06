61f71081a950c2c2e7d4477f6445bfa9c072b9f8
remusao <remusao@users.noreply.github.com>
2022-11-06 19:49:19 +0000

    Deploying to master from @ remusao/remusao.github.io@b4add64537ae9b1dd5431bf0216cdd0fb8c45235 🚀

--- snippets/dbm_bench.py
+++ snippets/dbm_bench.py
@@ -1,51 +1,53 @@
+#!/usr/bin/env python
 
-import dbm
-import sqlite3
-import os
-
+from pathlib import Path
 from random import random
+import dbm.gnu as dbm
+import sqlite3
 import time
 
 
 MAX_RECORDS = 10000
 WRITES = [str(i) for i in range(MAX_RECORDS)]
-RANDOM_READS = [
-    str(int(random() * MAX_RECORDS))
-    for _ in range(MAX_RECORDS)
-]
+RANDOM_READS = [str(int(random() * MAX_RECORDS)) for _ in range(MAX_RECORDS)]
+
 
 class SqliteDict:
     def __init__(self, con):
         self.con = con
-        self.con.execute('CREATE TABLE store(key TEXT, value TEXT)')
+        self.con.execute("CREATE TABLE store(key TEXT PRIMARY KEY, value TEXT)")
+        self.con.execute("PRAGMA journal_mode = WAL")
 
     def __setitem__(self, key, value):
         with self.con:
-            self.con.execute('INSERT INTO store VALUES (?,?)', (key, value))
+            self.con.execute("INSERT INTO store VALUES (?,?)", (key, value))
+
     def __getitem__(self, key):
         with self.con:
-            return self.con.execute('SELECT value FROM store WHERE key=?', (key,))
+            return self.con.execute("SELECT value FROM store WHERE key=?", (key,))
+
 
 def sqlite3_mem_open():
-    with sqlite3.connect(':memory:') as con:
+    with sqlite3.connect(":memory:") as con:
         yield SqliteDict(con)
+
 
 def sqlite3_file_open():
-    with sqlite3.connect('file.sql') as con:
+    with sqlite3.connect("file.sql") as con:
         yield SqliteDict(con)
-    try:
-        os.remove('file.sql')
-    except os.FileNoteFoundError:
-        print('Could not find', 'file.sql')
+
+    for file in Path("./").rglob("file.*"):
+        file.unlink()
+
 
 def dbm_open():
-    name = 'dbm.db'
-    with dbm.open('dbm', 'c') as db:
+    name = "dbm.db"
+    with dbm.open("dbm", "c") as db:
         yield db
-    try:
-        os.remove(name)
-    except os.FileNoteFoundError:
-        print('Could not find', name)
+
+    for file in Path("./").rglob("dbm"):
+        file.unlink()
+
 
 def dict_open():
     yield {}
@@ -57,33 +59,144 @@ def bench(db_gen):
 
         # create some records
         for i in WRITES:
-            db[i] = 'x'
+            db[i] = "x"
 
         # do a some random reads
         for i in RANDOM_READS:
             x = db[i]
 
         time_taken = time.time() - t
-        print("Took %0.3f seconds, %0.5f microseconds / record" % (time_taken, (time_taken * 1000000) / MAX_RECORDS))
+        print(
+            "took %0.3f seconds, %0.5f microseconds / record"
+            % (time_taken, (time_taken * 1000000) / MAX_RECORDS)
+        )
+
 
 def bench_sqlite():
-    with sqlite3.connect('db.sql') as con:
+    with sqlite3.connect("db.sql") as con:
         with con:
-            con.execute('CREATE TABLE store(key TEXT, value TEXT)')
+            con.execute("CREATE TABLE store(key TEXT PRIMARY KEY, value TEXT)")
+            con.execute("PRAGMA journal_mode = WAL")
         t = time.time()
         with con:
-            con.executemany('INSERT INTO store VALUES (?,?)', (
-                (k, 'x') for k in WRITES
-            ))
+            con.executemany(
+                "INSERT INTO store VALUES (?,?)", ((k, "x") for k in WRITES)
+            )
         with con:
-            con.execute('SELECT * FROM store WHERE key in ({0})'.format(', '.join('?' for _ in RANDOM_READS)), RANDOM_READS).fetchall()
+            con.execute(
+                "SELECT * FROM store WHERE key in ({0})".format(
+                    ", ".join("?" for _ in RANDOM_READS)
+                ),
+                RANDOM_READS,
+            ).fetchall()
         time_taken = time.time() - t
-        print("Took %0.3f seconds, %0.5f microseconds / record" % (time_taken, (time_taken * 1000000) / MAX_RECORDS))
+        print(
+            "took %0.3f seconds, %0.5f microseconds / record"
+            % (time_taken, (time_taken * 1000000) / MAX_RECORDS)
+        )
+
+    for file in Path("./").rglob("db.*"):
+        file.unlink()
+
+
+def bench_sqlite_memory():
+    with sqlite3.connect(":memory:") as con:
+        with con:
+            con.execute("CREATE TABLE store(key TEXT PRIMARY KEY, value TEXT)")
+            con.execute("PRAGMA journal_mode = WAL")
+        t = time.time()
+        with con:
+            con.executemany(
+                "INSERT INTO store VALUES (?,?)", ((k, "x") for k in WRITES)
+            )
+        with con:
+            con.execute(
+                "SELECT * FROM store WHERE key in ({0})".format(
+                    ", ".join("?" for _ in RANDOM_READS)
+                ),
+                RANDOM_READS,
+            ).fetchall()
+        time_taken = time.time() - t
+        print(
+            "took %0.3f seconds, %0.5f microseconds / record"
+            % (time_taken, (time_taken * 1000000) / MAX_RECORDS)
+        )
+
+
+def bench_sqlite_index_before_insertions():
+    with sqlite3.connect("db.sql") as con:
+        with con:
+            con.execute("CREATE TABLE store(key TEXT PRIMARY KEY, value TEXT)")
+            con.execute("PRAGMA journal_mode = WAL")
+            con.execute("CREATE INDEX key_index ON store(key)")
+        t = time.time()
+        with con:
+            con.executemany(
+                "INSERT INTO store VALUES (?,?)", ((k, "x") for k in WRITES)
+            )
+        with con:
+            con.execute(
+                "SELECT * FROM store WHERE key in ({0})".format(
+                    ", ".join("?" for _ in RANDOM_READS)
+                ),
+                RANDOM_READS,
+            ).fetchall()
+        time_taken = time.time() - t
+        print(
+            "took %0.3f seconds, %0.5f microseconds / record"
+            % (time_taken, (time_taken * 1000000) / MAX_RECORDS)
+        )
+
+    for file in Path("./").rglob("db.*"):
+        file.unlink()
+
+
+def bench_sqlite_index_after_insertions():
+    with sqlite3.connect("db.sql") as con:
+        with con:
+            con.execute("CREATE TABLE store(key TEXT PRIMARY KEY, value TEXT)")
+            con.execute("PRAGMA journal_mode = WAL")
+
+        t = time.time()
+        with con:
+            con.executemany(
+                "INSERT INTO store VALUES (?,?)", ((k, "x") for k in WRITES)
+            )
+
+        with con:
+            con.execute("CREATE INDEX key_index ON store(key)")
+
+        with con:
+            con.execute(
+                "SELECT * FROM store WHERE key in ({0})".format(
+                    ", ".join("?" for _ in RANDOM_READS)
+                ),
+                RANDOM_READS,
+            ).fetchall()
+        time_taken = time.time() - t
+        print(
+            "took %0.3f seconds, %0.5f microseconds / record"
+            % (time_taken, (time_taken * 1000000) / MAX_RECORDS)
+        )
+
+    for file in Path("./").rglob("db.*"):
+        file.unlink()
 
 
 if __name__ == "__main__":
-    bench_sqlite()
+    print("dict – ", end="")
     bench(dict_open)
+    print("dbm – ", end="")
     bench(dbm_open)
+    print("sqlite3 (executemany + :memory:) – ", end="")
+    bench_sqlite_memory()
+    print("sqlite3 (executemany + file) – ", end="")
+    bench_sqlite()
+    print("sqlite3 (executemany + index before insertions) – ", end="")
+    bench_sqlite_index_before_insertions()
+    print("sqlite3 (executemany + index after insertions) – ", end="")
+    bench_sqlite_index_after_insertions()
+    print("sqlite3 (:memory:) – ", end="")
     bench(sqlite3_mem_open)
+    print("sqlite3 (file)  – ", end="")
     bench(sqlite3_file_open)
